ff04c57cffee05f0f346e8ea067f7eb5e76c94e0
Caio Rezende <caiocezar50@outlook.com>
2024-02-24 20:34:38 -0300

    feat: Basic graphQL query

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("GraphQL App");
 });
+
+const root = {
+  hello: () => "Hi!",
+};
+
+app.use("/graphql", graphqlHTTP({ schema, rootValue: root, graphiql: true }));
 
 app.listen(8080, () => console.log("Running serevr on ::8080/graphql"));
