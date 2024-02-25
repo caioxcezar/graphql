ab7ab508f35a9d9267605f615ce2253cd5d67f76
Caio Rezende <caiocezar50@outlook.com>
2024-02-25 17:44:16 -0300

    feat: Add new types

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,14 @@
 const productDB = {};
 
 class Product {
-  constructor(id, { name, description, price, soldout, stores }) {
+  constructor(id, { name, description, price, soldout, stores, inventory }) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.soldout = soldout;
     this.stores = stores;
+    this.inventory = inventory;
   }
 }
 
